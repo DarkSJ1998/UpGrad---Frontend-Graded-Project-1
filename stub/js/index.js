f81bff5afdd111522206210ced739ab1b3a45a4b
DarkSJ1998 <sarthakjn12@gmail.com>
2019-03-17 21:34:51 +0530

    Added Create Post Modal and stuff

---
+++ stub/js/index.js
@@ -0,0 +1,10 @@
+var create = document.getElementById('createModal');
+hideCreateModal();
+
+function createModal() {
+    create.style.display = 'flex';
+}
+
+function hideCreateModal() {
+    create.style.display = 'none';
+}
